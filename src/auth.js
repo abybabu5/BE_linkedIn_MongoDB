f96bf96611639cb4f37c6214a9a177e38da1c7ce
Aby Babu <abyStriveschool@gmail.com>
2020-02-16 11:55:01 +0100

    oauth-fb

--- src/auth.js
+++ src/auth.js
@@ -6,6 +6,7 @@ const jwtOptions = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), //Authorization: Bearer TOKEN
     secretOrKey: process.env.TOKEN_PASSWORD //
 };
+
 module.exports = {
     getToken: (user) => jwt.sign(user, jwtOptions.secretOrKey, { expiresIn: 3600 }),
     jwtOptions //this is just an helper to have a central point for token generation
